a2f82e97f61e74ca605ca2304b08f1a18148e3cc
Eduardo Lawrenz <eduardolawrenz76@gmail.com>
2025-07-26 21:32:00 -0300

    Corrige conflito em index.php e restaura bootstrap padrão do Laravel

--- src/public/index.php
+++ src/public/index.php
@@ -1,38 +1,20 @@
 <?php
 
-<<<<<<< HEAD
-use Illuminate\Foundation\Application;
-=======
 use Illuminate\Contracts\Http\Kernel;
->>>>>>> d624a76 (Corrigindo diversos erros)
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-<<<<<<< HEAD
-// Determine if the application is in maintenance mode...
-=======
 // Verifica se está em modo de manutenção
->>>>>>> d624a76 (Corrigindo diversos erros)
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-<<<<<<< HEAD
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
-
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$app->handleRequest(Request::capture());
-=======
 // Autoload do Composer
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 // Bootstrapping do Laravel
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
 
 // Cria o kernel e trata a requisição
 $kernel = $app->make(Kernel::class);
@@ -42,4 +24,3 @@ $response = $kernel->handle(
 )->send();
 
 $kernel->terminate($request, $response);
->>>>>>> d624a76 (Corrigindo diversos erros)
